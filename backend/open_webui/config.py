92e76b6b7a2e042ccb9e3dba2105ca145ce740a5
o <uyena1hh2@gmail.com>
2026-03-31 13:08:07 +0700

    chore: set new signups as user roles

--- backend/open_webui/config.py
+++ backend/open_webui/config.py
@@ -1278,7 +1278,7 @@ DEFAULT_MODEL_PARAMS = PersistentConfig(
 DEFAULT_USER_ROLE = PersistentConfig(
     "DEFAULT_USER_ROLE",
     "ui.default_user_role",
-    os.getenv("DEFAULT_USER_ROLE", "pending"),
+    os.getenv("DEFAULT_USER_ROLE", "user"),
 )
 
 DEFAULT_GROUP_ID = PersistentConfig(
